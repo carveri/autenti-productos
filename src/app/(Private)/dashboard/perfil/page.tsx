b76177f515086af7f015dfc21e64e7515da01c30
carveri <carvajal.cortes.e19@gmail.com>
2024-03-08 10:12:36 -0300

    V5 Agregue mi propio form de login :)

--- src/app/(Private)/dashboard/perfil/page.tsx
+++ src/app/(Private)/dashboard/perfil/page.tsx
@@ -9,12 +9,22 @@ const page =() => {
     const {data: session} = useSession()
 
   return (
-    <div>
-        <section>
-            {session?.user?.name ?? 'as'}
-            {session?.user?.email ?? 'as'}
-            {session?.user?.id ?? 'as'}
-           
+    <div className='w-full h-full grid justify-items-center '>
+        <section className='w-2/6 h-3/6 bg-red-800 mt-48 px-8'>
+          <header className='h-16 text-2xl grid place-items-center'>
+            Perfil del Usuario
+          </header>
+            <section className='grid grid-rows-3 text-xl'>
+              <article>
+                Username: {session?.user?.name ?? 'as'}
+              </article>
+              <article>
+                Email: {session?.user?.email ?? 'as'}
+              </article>
+              <article>
+                Id: {session?.user?.id ?? 'as'}
+              </article>
+            </section>
         </section>
     </div>
   )
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 import prisma from "@/libs/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 
 
@@ -15,6 +16,35 @@ export const authOptions: NextAuthOptions = {
           clientId: process.env.GITHUB_ID ?? '',
           clientSecret: process.env.GITHUB_SECRET ?? '',
         }),
+
+        CredentialsProvider({
+          // The name to display on the sign in form (e.g. "Sign in with...")
+          name: "Credentials",
+          // `credentials` is used to generate a form on the sign in page.
+          // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+          // e.g. domain, username, password, 2FA token, etc.
+          // You can pass any HTML attribute to the <input> tag through the object.
+          credentials: {
+            email: { label: "Email", type: "email", placeholder: "jsmith@gmail.com" },
+            password: { label: "Contraseña", type: "password", placeholder: "***********" }
+          },
+          async authorize(credentials, req) {
+            // Add logic here to look up the user from the credentials supplied
+            const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+      
+            if (user) {
+              // Any object returned will be saved in `user` property of the JWT
+              return user
+            } else {
+              // If you return null then an error will be displayed advising the user to check their details.
+              return null
+      
+              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+            }
+          }
+        })
+
+        
       ],
 
 
@@ -33,8 +63,12 @@ export const authOptions: NextAuthOptions = {
               email: token.email ?? 'No hay email'
             }
           })
+          if(dbUser?.isActive === false){
+            throw Error('El usuario no esta activo!!')
+          }
 
           token.id = dbUser?.id ?? 'no-uuid'
+          
           
           return token
         },
